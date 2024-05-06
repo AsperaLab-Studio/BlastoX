65f6f8fdfc494628083cd57e64711b1f1a11e0a5
Alien089 <gabri.moratelli@gmail.com>
2024-05-06 22:18:35 +0200

    working on damageable boxes

--- assets/Scripts/Environment/DestroyObj.gd
+++ assets/Scripts/Environment/DestroyObj.gd
@@ -1,6 +1,18 @@
-extends Node
+class_name DamageableObj
+extends Node2D
 
-# Called when the node enters the scene tree for the first time.
+onready var sprite: Sprite = get_node("Sprite")
+export(int) var hp_box := 2
+
+var initial_frame
+
 func _ready():
-	pass # Replace with function body.
+    initial_frame = sprite.frame
 
+func _process(delta):
+    if sprite.frame == initial_frame + 2:
+        #spawn 1/3 obj
+        pass 
+
+func hit(dps, type, source):
+    sprite.frame = sprite.frame + dps
