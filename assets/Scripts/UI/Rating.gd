b42bc1ce9cd84ce1ae47b057b05ee7d2c2efa34d
Alien089 <gabri.moratelli@gmail.com>
2024-07-30 05:23:00 +0200

    fixed rating systen

--- assets/Scripts/UI/Rating.gd
+++ assets/Scripts/UI/Rating.gd
@@ -6,7 +6,6 @@ onready var stageManager = get_parent().get_parent().get_parent().get_node("Stag
 export(Array, StreamTexture) var rate_list
 export(bool) var boss = false
 
-var rate_points: Array
 var actual_rate
 
 func _ready():
@@ -14,36 +13,27 @@ func _ready():
 
 
 func _process(_delta):
-	calculate_points()
 	calculate_rate()
 
 	if stageManager.win.visible == true:
 		sprite.visible = true
-
-func calculate_points():
-	var maxPoints = stageManager.kill * 100
-	var pointsTheshold = maxPoints / 4
-	var t1 = pointsTheshold * 1
-	var t2 = pointsTheshold * 2
-	var t3 = pointsTheshold * 3
-
-	rate_points = [t1, t2, t3]
-	
+		
 
 func calculate_rate():
 	var score
+	var maxPoints = stageManager.kill * 100
 	if !boss:
 		score = stageManager.points
 	else:
 		score = Global.scoreZone
 
-	if score <= rate_points[0]:
-		actual_rate = 0
-	elif score <= rate_points[1]:
-		actual_rate = 1
-	elif score <= rate_points[2]:
-		actual_rate = 2
-	else:
+	if score == maxPoints:
 		actual_rate = 3
+	elif score >= 0.8 * maxPoints:
+		actual_rate = 2
+	elif score >= 0.5 * maxPoints:
+		actual_rate = 1
+	else:
+		actual_rate = 0
 		
 	sprite.texture = rate_list[actual_rate]
